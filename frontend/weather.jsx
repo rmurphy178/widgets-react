438012f6ce7e5e036988fdc87354833e88fda629
Ali Haq <ali.m.haq@gmail.com>
2017-07-07 17:05:47 -0700

    add weather api to widget

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -1,2 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class Weather extends React.Component {
+  constructor() {
+    this.state = {
+      weather: null
+    };
+
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.weatherAPI);
+  }
+
+  weatherAPI(pos) {
+    let lat = pos.coords.latitude;
+    let long = pos.coords.longitude;
+    let url =
+    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=cad7e62c32c1d3c24b237b3ac5dce35a";
+  }
+}
+
+export default Weather;
--- widgets.jsx
+++ widgets.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Congrats from './congrats';
 import Clock from './frontend/clock';
+import Weather from './frontend/weather';
 
 document.addEventListener("DOMContentLoaded", () => {
-	const root = document.getElementById("root");
-	ReactDOM.render(<Clock/>, root);
+	const clockRoot = document.getElementById("clockRoot");
+	const weatherRoot = document.getElementById("weatherRoot");
+	ReactDOM.render(<Clock/>, clockRoot);
+	ReactDOM.render(<Weather/>, weatherRoot);
 });
